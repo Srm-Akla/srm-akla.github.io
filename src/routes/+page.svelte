03aa4d50a6df9f33897c74d8faa70534be670126
srm-akla <sriramakella@protonmail.com>
2024-02-26 23:06:00 +0100

    removing snowfall effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,10 +22,10 @@
 	<title>{useTitle(title, TITLE_SUFFIX)}</title>
 </svelte:head>
 
+<!--<div class="snow"> </div>-->
 <div
 	class="col self-center flex-1 md:flex-row md:slef-stretch justify-center lg:justify-between items-center p-y-0px p-x-10px"
 >
-<div class="snow"> </div>
 	<div class="md:flex-1 gap-10px">
 		<MainTitle classes="md:text-left text-[var(--code-operator)]">Welcome!</MainTitle>
 		<p class="text-[var(--tertiary-text)]  text-center md:text-left text-[1.2em] font-extralight">
